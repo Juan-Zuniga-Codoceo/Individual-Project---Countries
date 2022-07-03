24d9d2959d09c490133c19ace2c60e9fd46e829c
Juan-Zuniga-Codoceo <zcja.89@gmail.com>
2022-07-03 04:25:33 -0400

    2

---
+++ api/src/modules/bulkActivityBD.js
@@ -0,0 +1,26 @@
+const { conn } = require("../db");
+const { Activity } = conn.models;
+
+const bulkActivityBD = async (para) => {
+    if(!para.country || !para.name) return null;
+    if(!await findActivity({ name: para.name })) {
+
+        await Activity.create({
+            name: para.name,
+            difficulty: para.difficulty,
+            duration: para.duration,
+            season: para.season
+        })
+    }
+    let reg = await findActivity({ name: para.name });
+    para.country.forEach(async(el)=>{
+        await reg.addCountry(el)
+    })
+}
+
+const findActivity = async (act)=>{
+    let reg = await Activity.findOne({ where: act });
+    return reg;
+}
+
+module.exports = bulkActivityBD;
