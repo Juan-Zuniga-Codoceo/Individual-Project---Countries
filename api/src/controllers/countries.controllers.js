8a8e0a82efd6417c382a68d89eb8eae12c00477d
Juan-Zuniga-Codoceo <zcja.89@gmail.com>
2022-07-04 22:03:06 -0400

    3

---
+++ api/src/controllers/countries.controllers.js
@@ -0,0 +1,40 @@
+const {
+    getCountries,
+    getCountryById,
+    getCountryByName,
+    getCountriesByName,
+} = require("../services/countries.services");
+
+const getCountriesController = async (req, res) => {
+    try {
+        if (req.query.name){
+            const countries = await getCountriesByName(req.query.name);
+            res.status(200).json(countries);
+        } else {
+            const countries = await getCountries();
+            res.status(200).json(countries);
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const getCountryByIdController = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const country = await getCountryById(id);
+        if (!country) {
+            throw new Error("Country not found");
+        }
+        res.json(country);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = {
+    getCountriesController,
+    getCountryByIdController,
+};
---
+++ api/src/models/Activity.js
@@ -0,0 +1,43 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize)=>{
+    sequelize.define(
+        "activity",
+        {
+            name : {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            difficulty : {
+                type: DataTypes.INTEGER,
+                validate: {
+                    min: 1,
+                    max: 5,
+                },
+                allowNull: false,
+            },
+            duration : {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            season: {
+                type: DataTypes.ARRAY(DataTypes.STRING),
+                validate: {
+                    isSeasons: (value) => {
+                        if (
+                            !value.every((season) =>
+                            ["spring","summer","autumn","winter"].includes(season)
+                            )
+                        ) {
+                            throw new Error("Invalid season");
+                        }
+                    },
+                },
+                allowNull: false,
+            },
+        },
+        {
+            timestamps: false,
+        }
+    );
+};
